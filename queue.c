88f22be6e797d1a79ff3eb5150fa1a4bfdfb3b67
reberu6 <zenschuu@gmail.com>
2025-03-11 14:35:21 +0800

    Implement q_free to release all queue memory
    - Added q_free() implementation in queue.c to free all queue storage.
    - Uses offsetof to compute element_t address from list_head pointer.
    
    Change-Id: Ib64e28f9e7e3d7ed7383d5d791068e566ad2c115

--- queue.c
+++ queue.c
@@ -16,7 +16,21 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    struct list_head *current = head->next;
+    while (current != head) {
+        struct list_head *next = current->next;
+        element_t *real_pos =
+            (element_t *) ((char *) current - offsetof(element_t, list));
+        free(real_pos->value);
+        free(real_pos);
+        current = next;
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
